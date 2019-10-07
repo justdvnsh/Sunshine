60321811906086eff36b0442acf02ea11b5d31fd
justdvnsh <justdvnsh2208@gmail.com>
2019-10-07 18:55:51 +0530

    added the weather data . Addded asyncTask to fetch the weatherdata. Building and loading the uri . Fetching the weather data, and showing it to the user. Addded a menu refersh button

--- app/src/main/java/com/example/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sunshine/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.sunshine;
 
+import android.net.Uri;
+import android.net.UrlQuerySanitizer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,11 +11,129 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView weatherData;
+    ProgressBar progressBar;
+    TextView errorMessage;
+    final static String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+    final static String PARAM_QUERY = "q";
+    final static String APP_ID = "appid";
+
+
+    public URL buildUri(String text) {
+
+        Uri builtUri = Uri.parse(BASE_URL).buildUpon().appendQueryParameter(PARAM_QUERY, text)
+                .appendQueryParameter(APP_ID, "7ae534c3dc8e5ffc15f22533a0f91e11").build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    };
+
+
+    public String getResponseFromHttpUrl(URL url) throws IOException {
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        try {
+
+            InputStream inputStream = connection.getInputStream();
+
+            Scanner scanner = new Scanner(inputStream);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+
+            if ( hasInput ) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.disconnect();
+        }
+
+        return null;
+
+    }
+
+    public class LoadWeatherData extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL url = urls[0];
+            String result = null;
+            try {
+
+                result = getResponseFromHttpUrl(url);
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            progressBar.setVisibility(View.INVISIBLE);
+            if ( s != null & !s.equals("") ) {
+                showJSONData();
+                weatherData.setText(s);
+            } else {
+                showErrorMessage();
+            }
+        }
+    }
+
+    public void showWeather() {
+
+        URL url = buildUri("London");
+        Toast.makeText(MainActivity.this, url.toString(), Toast.LENGTH_SHORT).show();
+        new LoadWeatherData().execute(url);
+
+    }
+
+    public void showJSONData() {
+        weatherData.setVisibility(View.VISIBLE);
+        errorMessage.setVisibility(View.INVISIBLE);
+    }
+
+    public void showErrorMessage() {
+        weatherData.setVisibility(View.INVISIBLE);
+        errorMessage.setVisibility(View.VISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +142,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        weatherData = (TextView) findViewById(R.id.weather_data);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        errorMessage = (TextView) findViewById(R.id.error_message);
+
     }
 
     @Override
@@ -46,8 +163,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_refresh) {
+            showWeather();
         }
 
         return super.onOptionsItemSelected(item);
