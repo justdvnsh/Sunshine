afc963221cd318900c7ec0d6b088bb7245c62c30
justdvnsh <justdvnsh2208@gmail.com>
2019-11-10 00:57:51 +0530

    added querying method in cotent provider

--- app/src/main/java/com/example/sunshine/data/sunshineContentProvider.java
+++ app/src/main/java/com/example/sunshine/data/sunshineContentProvider.java
@@ -2,22 +2,75 @@ package com.example.sunshine.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class sunshineContentProvider extends ContentProvider {
+
+    private weatherDbHelper mWeatherDbHelper;
+
+    public static final int CODE_WEATHER = 100;
+    public static final int CODE_WEATHER_WITH_DATE = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    public static UriMatcher buildUriMatcher() {
+        final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = weatherContract.CONTENT_AUTHORITY;
+
+        uriMatcher.addURI(authority, weatherContract.PATH_WEATHER, CODE_WEATHER);
+        uriMatcher.addURI(authority, weatherContract.PATH_WEATHER + "/#", CODE_WEATHER_WITH_DATE);
+
+        return uriMatcher;
+    }
+
     @Override
     public boolean onCreate() {
-        return false;
+        mWeatherDbHelper = new weatherDbHelper(getContext());
+        return true;
     }
 
     @Nullable
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
-        return null;
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+
+        final SQLiteDatabase db = mWeatherDbHelper.getWritableDatabase();
+        Cursor cursor = null;
+        int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case CODE_WEATHER:
+                cursor = db.query(weatherContract.weatherEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            case CODE_WEATHER_WITH_DATE:
+                String date = uri.getLastPathSegment();
+                String mSelection = " = ? ";
+                String[] mSelectionArgs = new String[]{date};
+                cursor = db.query(weatherContract.weatherEntry.TABLE_NAME,
+                        projection,
+                        mSelection,
+                        mSelectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown Uri Exception" + uri);
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
 
     @Nullable
