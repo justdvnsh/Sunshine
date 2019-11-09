13cbc4c2d499017177357daf4af112f6164b57de
justdvnsh <justdvnsh2208@gmail.com>
2019-11-09 23:17:51 +0530

    added a content provider class

--- app/src/main/java/com/example/sunshine/data/weatherDbHelper.java
+++ app/src/main/java/com/example/sunshine/data/weatherDbHelper.java
@@ -9,7 +9,7 @@ import androidx.annotation.Nullable;
 public class weatherDbHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "weather.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public weatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
